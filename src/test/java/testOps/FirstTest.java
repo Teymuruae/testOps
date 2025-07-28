4a52c5e2c5609e70a630111879201ee185582997
Баратов Теймур <baratov.teymur@yandex.ru>
2025-07-28 15:22:22 +0300

    FirstTest push

---
+++ src/test/java/testOps/FirstTest.java
@@ -0,0 +1,42 @@
+package testOps;
+
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class FirstTest {
+
+    @Test
+    void test1(){
+        step("Click enter button", () -> {
+
+        });
+
+        step("login", () -> {
+
+        });
+
+        step("check elements", () -> {
+
+        });
+
+        step("logout", () -> {
+
+        });
+    }
+
+    @Test
+    void test2(){
+        step("Send request", () -> {
+
+        });
+
+        step("check email", () -> {
+
+        });
+
+        step("check base", () -> {
+
+        });
+    }
+}
